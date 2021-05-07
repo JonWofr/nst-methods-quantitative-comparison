5467b57827eb1792d8778348ee47429c49e057fe
Jonas Wolfram <jw132@hdm-stuttgart.de>
2021-05-07 14:20:22 +0200

    Included gitignore and requirements file

--- scripts/resizer.py
+++ scripts/resizer.py
@@ -7,8 +7,6 @@ parser.add_argument('--images-dir-url', required=True,
                     help='The image directory in which the images are located which should be resized')
 parser.add_argument('--resized-images-dir-url', required=True,
                     help='The image directory in which the resized images should be stored')
-parser.add_argument('--resized-image-filename-prefix', required=False, default='resized-',
-                    help='An optional prefix for the resized images\' filenames')
 parser.add_argument('--size', type=int, required=True,
                     help='The size the image should be resized to. Note: Only the smaller side of the image is resized to this number and the other side is resized so that the aspect ratio is preserved')
 args = parser.parse_args()
@@ -28,5 +26,5 @@ for image_filename in images_filenames:
     resized_images_dir_url = args.resized_images_dir_url
     os.makedirs(resized_images_dir_url, exist_ok=True)
     resized_image_file_url = os.path.join(
-        resized_images_dir_url, f'{args.resized_image_filename_prefix}{image_filename}')
+        resized_images_dir_url, image_filename)
     resized_image.save(resized_image_file_url)
