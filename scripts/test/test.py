d1f409ab7821fe94e4d3dd2f18c6ac722543e650
Jonas Wolfram <jonas-wolfram@web.de>
2021-05-09 23:08:55 +0200

    Changed script so that pytorch-wct is working

--- scripts/test/test.py
+++ scripts/test/test.py
@@ -1,18 +1,22 @@
 import os
 import argparse
 import time
+import shutil
 
 
 def main():
+    method_names = ['collaborative-distillation', 'fast-neural-style', 'pytorch-adain', 'pytorch-neural-style-transfer', 'pytorch-wct']
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--method-name', required=True, choices=['collaborative-distillation', 'fast-neural-style', 'pytorch-adain', 'pytorch-neural-style-transfer', 'pytorch-wct'], help='The name of the NST method for which the stylized-images should be generated.')
+    parser.add_argument('--method-name', required=True, choices=method_names, help='The name of the NST method for which the stylized-images should be generated.')
     parser.add_argument('--method-dir-url', required=True, help='The url to the local repository of the NST method. Can be relative or absolute.')
     args = parser.parse_args()
     if args.method_name == 'collaborative-distillation':
-        # The PYTHONPATH environment variable has to be appended with the url of the collaborative-distillation method. The reason for that is that collaborative-distillation tries to import modules from parent directories inside child directories.
-        os.environ['PYTHONPATH'] = f"{os.environ['PYTHONPATH']}{os.pathsep}{os.path.abspath(args.method_dir_url)}" if 'PYTHONPATH' in os.environ else os.path.abspath(args.method_dir_url)
         # This method has to be exectued from the subdirectory 'PytorchWCT'
         args.method_dir_url = os.path.join(args.method_dir_url, 'PytorchWCT')
+    
+    # Creation of the directory in which all stylized-images of that particular method will be stored
+    os.makedirs(os.path.join('..', '..', 'nst-methods', args.method_name, 'stylized-images'), exist_ok=True)
     stylize_images(args)
 
 
@@ -44,6 +48,11 @@ def stylize_images(args):
         style_image_file_url = os.path.join(style_images_dir_url, style_image_filename)
         stylized_image_file_url = os.path.join(stylized_images_dir_url, stylized_image_filename)
 
+        # The method pytorch-wct needs special preprocessing
+        if args.method_name == 'pytorch-wct':
+            content_image_file_url, style_image_file_url = preprocess_pytorch_wct(content_image_file_url, style_image_file_url, stylized_images_dir_url)
+
+        # Each implementation requires a slightly different command to be executed
         command = parse_command(args.method_name, content_image_file_url, style_image_file_url, stylized_image_file_url, fast_neural_style_trained_models_dir_url)
 
         print(f'\n\nStylizing content-style-image-pair number {content_style_image_pair_index}...')
@@ -57,6 +66,10 @@ def stylize_images(args):
         times_file.write(f'\n{content_style_image_pair_index},{start_time_in_ms},{end_time_in_ms},{duration_in_ms}')
         print('\nTime needed (in ms):', duration_in_ms)
 
+        # The method pytorch-wct needs special postprocessing
+        if args.method_name == 'pytorch-wct':
+            postprocess_pytorch_wct(stylized_images_dir_url)
+
         # Some methods do not provide the option to give the stylized-image a custom name. In order to compare the methods with each other each stylized-image has to follow the same naming convention.
         if args.method_name == 'collaborative-distillation' or args.method_name == 'pytorch-adain' or args.method_name == 'pytorch-wct':
             print('stylized-image file need renaming. Renaming...')
@@ -67,7 +80,18 @@ def stylize_images(args):
     times_file.close()
 
 
-# Each implementation requires a slightly different command to be executed
+def preprocess_pytorch_wct(content_image_file_url, style_image_file_url, stylized_images_dir_url):
+    temp_content_image_dir_url = os.path.join(stylized_images_dir_url, 'temp', 'content-image')
+    temp_style_image_dir_url = os.path.join(stylized_images_dir_url, 'temp', 'style-image')
+    temp_content_image_file_url = os.path.join(temp_content_image_dir_url, 'in.jpg')
+    temp_style_image_file_url = os.path.join(temp_style_image_dir_url, 'in.jpg')
+    os.makedirs(temp_content_image_dir_url)
+    os.makedirs(temp_style_image_dir_url)
+    shutil.copyfile(content_image_file_url, temp_content_image_file_url)
+    shutil.copyfile(style_image_file_url, temp_style_image_file_url)
+    return temp_content_image_file_url, temp_style_image_file_url
+
+
 def parse_command(method_name, content_image_file_url, style_image_file_url, stylized_image_file_url, fast_neural_style_trained_models_dir_url):
     command = {
         'collaborative-distillation': f'python WCT.py --contentPath {os.path.dirname(content_image_file_url)} --stylePath {os.path.dirname(style_image_file_url)} --picked_content_mark {os.path.basename(content_image_file_url)} --picked_style_mark {os.path.basename(style_image_file_url)} --outf {os.path.dirname(stylized_image_file_url)} --mode 16x',
@@ -77,6 +101,10 @@ def parse_command(method_name, content_image_file_url, style_image_file_url, sty
         'pytorch-wct': f'python WCT.py --contentPath {os.path.dirname(content_image_file_url)} --stylePath {os.path.dirname(style_image_file_url)} --cuda --fineSize 0 --outf {os.path.dirname(stylized_image_file_url)}'
     }
     return command[method_name]
+
+
+def postprocess_pytorch_wct(stylized_images_dir_url):
+    shutil.rmtree(os.path.join(stylized_images_dir_url, 'temp'))
 
 
 def rename_stylized_image(method_name, stylized_images_dir_url, stylized_image_filename):
